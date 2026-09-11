f7ccac917ce749e9ff3045114d6d57a312427c0d
Henrique <goularthenrique9781@gmail.com>
2026-09-10 21:37:28 -0300

    Aula 10/09/2026

--- front/app/(sistema)/usuarios/page.tsx
+++ front/app/(sistema)/usuarios/page.tsx
@@ -1,4 +1,29 @@
+'use client'
+
+import { Usuario } from "@/app/types/usuario";
+import axios from "@/node_modules/axios/index";
+import { useEffect, useState } from "react";
+
 export default function Usuarios() {
+
+    const [usuarios, setUsuarios] = useState<Usuario[]>([]);
+
+    useEffect(() => {
+        carregarDados();
+    }, []);
+
+    const carregarDados = async () => {
+
+        try {
+            const dados = await axios.get<Usuario[]>("http://localhost:8080/usuarios");
+
+            setUsuarios(dados.data);
+
+        } catch (error) {
+            alert("Erro ao carregar usuarios!")
+        }
+    };
+
     return (
         <div className="space-y-6">
             <h1 className="text-2xl font-bold text-teal-800 tracking-tight">
@@ -8,7 +33,7 @@ export default function Usuarios() {
                 <thead>
                     <tr className="bg-teal-50">
                         <th className="px-4 py-3 text-left text-sm font-semibold text-teal-800">
-                            ID
+                            Código
                         </th>
                         <th className="px-4 py-3 text-left text-sm font-semibold text-teal-800">
                             Nome
@@ -25,23 +50,37 @@ export default function Usuarios() {
                     </tr>
                 </thead>
                 <tbody>
-                    <tr className="border-t border-teal-100 hover:bg-teal-50/50 transition-colors">
-                        <td className="px-4 py-3 text-sm text-gray-700">
-                            1
-                        </td>
-                        <td className="px-4 py-3 text-sm text-gray-700">
-                            Henrique
-                        </td>
-                        <td className="px-4 py-3 text-sm text-gray-700">
-                            123.456.789-00
-                        </td>
-                        <td className="px-4 py-3 text-sm text-gray-700">
-                            henrique@email.com
-                        </td>
-                        <td className="px-4 py-3 text-sm text-gray-700">
-                            Ativo
-                        </td>
-                    </tr>
+
+                    {usuarios.map((usuario) => (
+
+                        <tr key={usuario.id} className="border-t border-teal-100 hover:bg-teal-50/50 transition-colors">
+                            <td className="px-4 py-3 text-sm text-gray-700">
+                                {usuario.id}
+                            </td>
+                            <td className="px-4 py-3 text-sm text-gray-700">
+                                {usuario.nome}
+                            </td>
+                            <td className="px-4 py-3 text-sm text-gray-700">
+                                {usuario.cpf}
+                            </td>
+                            <td className="px-4 py-3 text-sm text-gray-700">
+                                {usuario.email}
+                            </td>
+                            <td className="px-4 py-3 text-sm text-gray-700">
+                                {usuario.status}
+                            </td>
+                        </tr>
+                    ))}
+
+                    {usuarios.length === 0 && (
+                        <tr>
+                            <td colSpan={5} className="px-6 py-12 text-center text-slate-800 italic">
+                                Nenhum usuario encontrado!
+                            </td>
+                        </tr>
+                    )
+                    }
+
                 </tbody>
             </table>
         </div>
