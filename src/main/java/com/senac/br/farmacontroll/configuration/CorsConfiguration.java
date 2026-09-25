c8c7e25a4508dfd53a390606700a5570e0a02448
Henrique <goularthenrique9781@gmail.com>
2026-09-24 21:20:51 -0300

    Aula 18/09/2026

--- src/main/java/com/senac/br/farmacontroll/configuration/CorsConfiguration.java
+++ src/main/java/com/senac/br/farmacontroll/configuration/CorsConfiguration.java
@@ -11,6 +11,15 @@ public class CorsConfiguration implements WebMvcConfigurer {
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
                 .allowedOrigins("http://localhost:3000")
-                .allowedMethods("GET","POST","PUT","OPTIONS","PATCH","HEAD");
+                .allowedMethods(
+                        "GET",
+                        "POST",
+                        "PUT",
+                        "DELETE",
+                        "OPTIONS",
+                        "PATCH",
+                        "HEAD"
+                );
     }
 }
+
